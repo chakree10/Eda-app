588428311318874df3dbe5486d0417769c5b3dd0
chakradar <chakradar10@hotmail.com>
2021-05-23 17:31:01 +0530

    Add files via upload

--- eda.py
+++ eda.py
@@ -20,6 +20,12 @@ with st.sidebar.header('1. Upload your CSV data'):
 [Example CSV input file](https://raw.githubusercontent.com/chakree10/EDA-web-app/main/Boston.csv)
 """)
 
+link1 = '[Regression](https://share.streamlit.io/chakree10/regression/main/regressor.py)'
+st.markdown(link1, unsafe_allow_html=True)
+
+link2 = '[Classification](https://share.streamlit.io/chakree10/classification/main/classifier.py)'
+st.markdown(link1, unsafe_allow_html=True)
+
 # Pandas Profiling Report
 if uploaded_file is not None:
     @st.cache
